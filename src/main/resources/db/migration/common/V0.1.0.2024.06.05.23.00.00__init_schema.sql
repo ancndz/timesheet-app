55c190b51d1183eaaef25846c6d64ef30e452a27
antonu <ytkaev12@live.com>
2024-06-06 00:00:31 +0300

    Добавил поддержку flyway

--- src/main/resources/db/migration/common/V0.1.0.2024.06.05.23.00.00__init_schema.sql
+++ src/main/resources/db/migration/common/V0.1.0.2024.06.05.23.00.00__init_schema.sql
@@ -11,17 +11,17 @@ create table timetable_cooperate_info
 
 create table timetable_notification
 (
-    type        integer,
-    archived_at timestamp(6),
-    created_at  timestamp(6),
-    dtype       varchar(31)  not null,
-    id          varchar(255) not null,
-    message     varchar(255),
-    sender_id   varchar(255),
-    status      varchar(255),
-    user_id     varchar(255),
+    type                     smallint check (type between 0 and 1),
+    archived_at              timestamp(6),
+    created_at               timestamp(6),
+    dtype                    varchar(31)  not null,
     cooperate_info_client_id varchar(255),
     cooperate_info_worker_id varchar(255),
+    id                       varchar(255) not null,
+    message                  varchar(255),
+    sender_id                varchar(255),
+    status                   varchar(255),
+    user_id                  varchar(255),
     primary key (id)
 );
 
@@ -56,24 +56,28 @@ create table timetable_users
 
 create table user_authorities
 (
-    authorities varbinary(255),
-    user_id     varchar(255) not null
+    user_id     varchar(255) not null,
+    authorities bytea
 );
 
 alter table if exists timetable_cooperate_info
     add constraint timetable_cooperate_info_client_fkey foreign key (client_id) references timetable_user_info;
+
 alter table if exists timetable_cooperate_info
     add constraint timetable_cooperate_info_worker_fkey foreign key (worker_id) references timetable_user_info;
 
 alter table if exists timetable_notification
     add constraint timetable_notification_sender_fkey foreign key (sender_id) references timetable_user_info;
+
 alter table if exists timetable_notification
     add constraint timetable_notification_user_fkey foreign key (user_id) references timetable_user_info;
+
 alter table if exists timetable_notification
     add constraint timetable_notification_coop_info_fkey foreign key (cooperate_info_client_id, cooperate_info_worker_id) references timetable_cooperate_info;
 
 alter table if exists timetable_schedule
     add constraint timetable_schedule_client_fkey foreign key (client_id) references timetable_user_info;
+
 alter table if exists timetable_schedule
     add constraint timetable_schedule_worker_fkey foreign key (worker_id) references timetable_user_info;
 
